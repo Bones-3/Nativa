9ea329025c91b1cdeddb082a9a5c56bfb3c49d2e
Fabián J <fa.jarao@duocuc.cl>
2026-06-12 22:03:36 -0400

    feat: se eliminan lo @repository.

--- auth-service/src/main/java/com/example/auth_service/repository/CredencialRepository.java
+++ auth-service/src/main/java/com/example/auth_service/repository/CredencialRepository.java
@@ -3,11 +3,9 @@ package com.example.auth_service.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.auth_service.model.Credencial;
 
-@Repository
 public interface CredencialRepository extends JpaRepository<Credencial, Long> {
     Optional<Credencial> findByCorreoUser(String correoUser); 
 
--- auth-service/src/main/java/com/example/auth_service/repository/TokenRepository.java
+++ auth-service/src/main/java/com/example/auth_service/repository/TokenRepository.java
@@ -1,11 +1,10 @@
 package com.example.auth_service.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import com.example.auth_service.model.Token;
 
-@Repository
 public interface TokenRepository extends JpaRepository<Token, Long>{
 
 }
