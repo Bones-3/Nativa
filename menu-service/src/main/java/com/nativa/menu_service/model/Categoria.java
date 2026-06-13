11783b0a3ac40a3dcb6313c29ffed1a7f85b0c30
Fabián J <fa.jarao@duocuc.cl>
2026-06-12 22:09:49 -0400

    feat: se recupera e integra menu-service en main

---
+++ menu-service/src/main/java/com/nativa/menu_service/model/Categoria.java
@@ -0,0 +1,30 @@
+package com.nativa.menu_service.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categoria")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Categoria {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Long id;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "disponible")
+    private Boolean disponible;
+}
---
+++ menu-service/src/main/java/com/nativa/menu_service/service/CategoriaService.java
@@ -0,0 +1,70 @@
+package com.nativa.menu_service.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.nativa.menu_service.dto.CategoriaRequest;
+import com.nativa.menu_service.dto.CategoriaResponse;
+import com.nativa.menu_service.mapper.CategoriaMapper;
+import com.nativa.menu_service.repository.CategoriaRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+//Lombok se encarga de generar el constructor con los argumentos necesarios para inyectar las dependencias
+@RequiredArgsConstructor
+public class CategoriaService {
+
+    private final CategoriaRepository categoriaRepository;
+    private final CategoriaMapper categoriaMapper;
+    
+    @Transactional(readOnly = true)
+    public List <CategoriaResponse> getCategoriasDisponible() {
+        return categoriaRepository.findByDisponibleTrue()
+                .stream()
+                .map(categoriaMapper::toResponse)
+                .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List <CategoriaResponse> getAllCategorias() {
+        return categoriaRepository.findAll()
+                .stream()
+                .map(categoriaMapper::toResponse)
+                .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public CategoriaResponse getCategoriaById(Long id) {
+        return categoriaRepository.findById(id)
+                .map(categoriaMapper::toResponse)
+                .orElseThrow(() -> new RuntimeException("Categoría no encontrada"));
+    }
+
+    @Transactional
+    public CategoriaResponse createCategoria(CategoriaRequest request) {
+        var categoria = categoriaMapper.toEntity(request);
+        return categoriaMapper.toResponse(categoriaRepository.save(categoria));
+    }
+
+    @Transactional
+    public CategoriaResponse updateCategoria(Long id, CategoriaRequest request) {
+        var categoria = categoriaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Categoría no encontrada"));
+
+        categoria.setNombre(request.getNombre());
+
+        return categoriaMapper.toResponse(categoriaRepository.save(categoria));
+    }
+
+    @Transactional
+    public void deleteCategoria(Long id) {
+        var categoria = categoriaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Categoría no encontrada"));
+
+        categoria.setDisponible(false);
+        categoriaRepository.save(categoria);
+    }
+}
